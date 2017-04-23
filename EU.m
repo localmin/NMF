e47916940fc1a074fc294de3f88ac4fe5c6148a3
localmin <localmin9201009@gmail.com>
2017-04-23 15:57:25 +0900

    brush up code of iteration for speed up

--- EU.m
+++ EU.m
@@ -28,33 +28,23 @@ for lp=1:itr
 
   tmpT = T;
   tmpV = V;
-  
-  % update T
-  for i=1:I
-    for k=1:K
-      up = 0;
-      low = 0;
-      for j=1:J
-        up = up + V(k,j) * X(i,j);
-        low = low + V(k,j) * Xf(i,j);
-      end
-      tmpT(i,k) = T(i,k) * up / low;
-    end
-  end
-  
-  % update V
+ 
+
   for k=1:K
-    for j=1:J
-      up = 0;
-      low = 0;
-      for i=1:I
-        up = up + T(i,k) * X(i,j);
-        low = low + T(i,k) * Xf(i,j);
-      end
-      tmpV(k,j) = V(k,j) * up / low;
-    end
+  	% update T
+
+  	up = X * V(k,:)';
+  	low = Xf * V(k,:)';
+
+  	tmpT(:,k)= T(:,k) .* (up ./ low);
+
+  	% update V
+	up = T(:,k)' * X;
+  	low = T(:,k)'* Xf;
+
+  	tmpV(k,:)= V(k,:) .* (up ./ low);
   end
-  
+
   % replace variable
   V = tmpV; T = tmpT;
   % update Xf
