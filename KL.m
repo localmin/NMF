ff0891afdb35ceda0f1046c5bc95e7e98394babd
localmin <localmin9201009@gmail.com>
2017-05-07 16:11:56 +0900

    fix vib of graph & brush up code of KL

--- KL.m
+++ KL.m
@@ -15,35 +15,23 @@ V = abs(randn( K, J ));
 
 % avoid vibration 
 Xf = T * V;
-up = 0;
-low = 0;
-
-up = X(:)' * Xf(:);
-low = Xf(:)' * Xf(:);
-
-cf = sqrt( up / low );
-T = T * cf;
-V = V * cf;
-Xf = Xf * cf * cf;
 
 One = ones(I,J);
 % Iteration by MU
 for lp=1:itr
 
-  tmpT = T.*( ((X./max(Xf,1e-12))*V') ./repmat(sum(V,2)',I,1));
+  T = T.*( ((X./max(Xf,1e-12))*V') ./repmat(sum(V,2)',I,1));
+  
+  Xf = T * V;
+  V = V.*( (T'*(X./max(Xf,1e-12))) ./repmat(sum(T,1)',1,J));
 
-  tmpV = V.*( (T'*(X./max(Xf,1e-12))) ./repmat(sum(T,1)',1,J));
-
-  % replace variables
-  V = tmpV; T = tmpT;
   % update Xf
   Xf = T * V;
 
   exe_time = exe_time + toc;
   
   % make KL-divergece
-  kl = (X .* log(X ./ Xf)) - X + Xf;
-  KL = reshape( kl, I*J, 1 );
+  KL = sum(sum(max(0,X.*log((X+1e-64)./(Xf+1e-64))-X+Xf)));
   error = norm( KL, 1 );
   
   wrt(lp,:) = [ exe_time  error ];
