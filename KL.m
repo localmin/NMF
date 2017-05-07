1fa50d1599b110687425849bb7e04666a867d06a
localmin <localmin9201009@gmail.com>
2017-05-07 15:44:53 +0900

    fix KL

--- KL.m
+++ KL.m
@@ -33,21 +33,9 @@ for lp=1:itr
   tmpT = T;
   tmpV = V;
 
-  upS = X ./ Xf;
+  tmpT = T.*( ((X./max(Xf,1e-12))*V') ./repmat(sum(V,2)',I,1));
 
-  for k=1:K
-    % update T
-    up = upS * V(k,:)';
-    low = One * V(k,:)';
-
-    tmpT(:,k)= T(:,k) .* (up ./ low);
-
-    % update V
-    up = T(:,k)' * upS;
-    low = T(:,k)'* One;
-
-    tmpV(k,:)= V(k,:) .* (up ./ low);
-  end
+  tmpV = V.*( (T'*(X./max(Xf,1e-12))) ./repmat(sum(V,1)',1,J));
 
   % replace variables
   V = tmpV; T = tmpT;
