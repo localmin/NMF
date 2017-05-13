c93ab9b385cf1867dc28d790fd8cc6be35c369d9
localmin <localmin9201009@gmail.com>
2017-05-13 19:31:54 +0900

    fix details of IS.m

--- IS.m
+++ IS.m
@@ -20,19 +20,17 @@ One = ones(I,J);
 % Iteration by MU
 for lp=1:itr
 
-  T = T.*( ((X.*max(1e-12,Xf).^-2)*V')./(max(1e-12,Xf).^-1*V') );
+  T = T.*sqrt(((X.*Xf.^-2)*V')./(Xf.^-1*V') );
 	
   Xf = T * V;	
-	V = V.*( (T'*(X.*max(1e-12,Xf).^-2))./(T'*max(1e-12,Xf).^-1) );
+	V = V.*sqrt((T'*(X.*Xf.^-2))./(T'*Xf.^-1) );
   
-  % update Xf
   Xf = T * V;
   
   exe_time = exe_time + toc;
   
   % make IS-divergence
-  IS = sum(sum(max(0,max(1e-12,X)./max(1e-12,Xf)-log(max(1e-12,X)./max(1e-12,Xf))-1)));
-  error = norm( IS, 1 );
+  error = sum(sum(X./Xf-log(X./Xf)-1));
   
   wrt(lp,:) = [ exe_time  error ];
   
