42f349b12b0796bcdeb7920e4865473c03d72fd0
localmin <localmin9201009@gmail.com>
2017-05-13 19:47:29 +0900

    fix iterration of MU

--- EU.m
+++ EU.m
@@ -11,39 +11,25 @@ wrt = zeros( itr , 2 );
 % random initialization
 T = abs(randn( I, K ));
 V = abs(randn( K, J ));
-
-% avoid vibration 
+ 
 Xf = T * V;
-
-up = X(:)' * Xf(:);
-low = Xf(:)' * Xf(:);
-
-cf = sqrt( up / low );
-T = T * cf;
-V = V * cf;
-Xf = Xf * cf * cf;
 
 % Iteration by MU
 for lp=1:itr
-
-  tmpT = T;
-  tmpV = V;
  
   % update T
-  tmpT = T .* ((X * V') ./ (Xf * V'));
-  	
+  T = T .* ((X * V') ./ (Xf * V'));
+  
+  Xf = T * V;
   % update V
- 	tmpV = V .* ((T' * X) ./ (T' * Xf));
+ 	V = V .* ((T' * X) ./ (T' * Xf));
 
-  % replace variable
-  V = tmpV; T = tmpT;
-  % update Xf
   Xf = T * V;
   
   
   exe_time = exe_time + toc;
   
-  error = norm( X - Xf, 'fro' );
+  error = norm( X - Xf, 'fro' )^2;
   
   wrt(lp,:) = [ exe_time  error ];
   
